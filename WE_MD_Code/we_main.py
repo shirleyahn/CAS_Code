0d14facc7441255bd7a63122728cedcff382cc13
Shirley Ahn <sahn1@stanford.edu>
2015-08-26 14:49:44 -0700

    separate codes for MD and toy models

---
+++ WE_MD_Code/we_main.py
@@ -0,0 +1,71 @@
+import sys
+main_directory = '/scratch/users/sahn1/WE_Triazine'  # TODO: set main directory for WE simulation
+sys.path.append(main_directory)
+import os
+os.chdir(main_directory)
+import numpy as np
+import we_global_variables as gv
+import we_functions
+from time import time
+
+
+def weighted_ensemble_simulation(input_parameters_file, input_initial_values_file):
+    # set simulation parameters
+    we_functions.set_parameters(input_parameters_file)
+
+    # create python objects for walkers and balls
+    walker_list = [None]*(gv.max_num_balls*gv.num_walkers)
+    temp_walker_list = [None]*(gv.max_num_balls*gv.num_walkers)
+    vacant_walker_indices = []
+    balls = np.zeros((1, gv.num_cvs+1))
+    ball_to_walkers = {}
+
+    # create walkers and their directories
+    we_functions.initialize(input_initial_values_file, walker_list, temp_walker_list, ball_to_walkers,
+                            vacant_walker_indices)
+
+    for step_num in range(gv.initial_step_num, gv.initial_step_num + gv.max_num_steps):
+        # reset ball objects so that balls are newly created at every step
+        if gv.balls_flag == 0 and step_num != gv.initial_step_num:
+            balls = np.zeros((1, gv.num_cvs+1))
+            ball_to_walkers = {}
+            gv.current_num_balls = 0
+
+        if gv.flag == 1 and step_num == gv.initial_step_num:
+            pass
+        else:
+            gv.first_walker = 0
+            gv.last_walker = gv.num_occupied_balls*gv.num_walkers-1
+        print 'running   ' + str(step_num+1) + '-th step'
+        os.chdir(gv.main_directory)
+        f = open('bash_script_input_file.txt', 'w')
+        f.write(str(gv.first_walker))
+        f.write(' first_' + str(gv.last_walker) + '_last')
+        f.close()
+
+        # first, run simulation with bash script
+        t0 = time()
+        if (gv.flag == 2 or gv.flag == 3) and step_num == gv.initial_step_num:
+            pass
+        else:
+            os.system('./we_simulations.sh')
+
+        # second, create balls and assign walkers to balls
+        t1 = time()
+        new_balls = we_functions.binning(step_num, walker_list, temp_walker_list, balls, ball_to_walkers)
+
+        # third, resample walkers for every ball
+        we_functions.resampling(walker_list, temp_walker_list, new_balls, ball_to_walkers, vacant_walker_indices)
+
+        # finally, output the results in text files
+        we_functions.print_status(step_num, walker_list, new_balls, ball_to_walkers)
+        balls = new_balls
+        t2 = time()
+
+        os.chdir(gv.main_directory+'/WE')
+        f = open('time_record.txt', 'a')
+        f.write(str(step_num+1) + '-th step: ' + 'simulation time: ' + str(t1-t0) + ' ' + 'post-processing time: ' +
+                str(t2-t1) + '\n')
+        f.close()
+        
+weighted_ensemble_simulation('we_parameters.txt', 'we_initial_values.txt')
---
+++ WE_MD_Code/we_simulations.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+###
+# TODO: set main directory, walker directory, and molecular dynamics simulation program directory
+# and number of nodes requested, number of cores per node, number of gpu's requested.
+# num_sim_walkers should be equal to num_nodes * num_cpu, since one walker will run with one node.
+###
+
+export MAIN_DIRECTORY=/scratch/users/sahn1/WE_Triazine
+export WALKER_DIRECTORY=/scratch/users/sahn1/WE_Triazine/WE
+export GROMACS=/home/sahn1/gromacs/4.6.4/bin
+
+num_nodes=1
+num_cpu=16
+num_gpu=8
+num_sim_walkers=16
+
+# get sequence of walker indices from sh_input.txt
+cd $MAIN_DIRECTORY
+output=$(cat bash_script_input_file.txt)
+first_walker=$(echo $output | awk '{$NF=""}1')
+second_walker=$(echo $output | cut -d'_' -f 2)
+
+# assume walkers run in separate directories, walker0, walker1, ...
+cd $WALKER_DIRECTORY
+counter=0
+pin=0
+for i in `seq $first_walker $last_walker`;
+do
+	if [ $counter -lt $num_sim_walkers ];
+	then
+		cd walker$i/
+                let cpu_pin=($i%$num_cpu) #pin=$(($i/$num_nodes))
+                let gpu_pin=($i%$num_gpu)
+                # write hostfile for i-th job to use
+                let lstart=($counter-1)*${num_nodes}+2
+                let lend=${lstart}+${num_nodes}-1
+                sed -n ${lstart},${lend}'p' < ../../nodefilelist.txt > nodefile$counter
+                ssh $(cat nodefile$counter) bash -c "`
+                cd ${WALKER_DIRECTORY}
+
+                cd walker$i
+
+                export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sahn1/
+
+                ${GROMACS}/grompp -f ../../prod.mdp -p ../../topol_ions.top -n ../../index.ndx -c minim.gro -o run.tpr -maxwarn 1
+
+                ${GROMACS}/mdrun -gpu_id $gpu_pin -ntmpi 1 -ntomp 1 -pin on -pinstride 1 -pinoffset $cpu_pin -v -deffnm run
+                `" &
+                echo "running walker$i"
+                cd ..
+                let counter=counter+1
+	fi
+  	if [ $counter == $num_sim_walkers ];  # limit to num_sim_walkers concurrent walkers
+  	then
+      		let counter=0 # reset counter
+      		wait
+  	fi
+done
+wait
+
+# post-process data after simulations are done. this needs to be modified depending on how the output files are named
+# and what collective variables are being collected from the simulations.
+for i in `seq $first_walker $last_walker`;
+do
+    cd walker$i
+    echo "post-processing walker$i"
+    while [ ! -f "run.gro" ];  # in case a simulation failed to run
+    do
+        sed -n 1,1'p' < ../../nodefilelist.txt > nodefile0
+        ssh $(cat nodefile0) bash -c "`
+        cd ${WALKER_DIRECTORY}
+
+        cd walker$i
+
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sahn1/
+
+        ${GROMACS}/grompp -f ../../prod.mdp -p ../../topol_ions.top -n ../../index.ndx -c minim.gro -o run.tpr -maxwarn 1
+
+        ${GROMACS}/mdrun -v -deffnm run
+        `" &
+    done
+    wait
+    rm -rf nodefile*
+    rm -rf mdout.mdp
+    mv run.gro minim.gro
+    echo 0 | ${GROMACS}/trjconv -s ../../init.gro -f run.xtc -b 10.0 -o run.xtc
+    ${GROMACS}/g_angle -f run.xtc -n ../../dihedrals.ndx -ov -all -type dihedral
+    tail -1 angaver.xvg > coordinates.out
+    echo 22 23 | ${GROMACS}/g_mindist -s ../../init.gro -f run.xtc -n ../../index.ndx 
+    awk 'END {print $2*10}' mindist.xvg >> coordinates.out
+    awk 'NR%2{printf $0" ";next;}1' coordinates.out > new_coordinates.out
+    mv new_coordinates.out coordinates.out
+    rm -rf ang*
+    rm -rf mindist*
+    if [ -f "traj.xtc" ];  # concatenating trajectories after the first step
+    then
+             ${GROMACS}/trjcat -f traj.xtc run.xtc -o out.xtc -cat
+    fi
+    mv run.xtc traj.xtc
+    mv out.xtc traj.xtc
+    rm -rf run*
+    rm -rf \#*
+    cd ..
+done
+exit
