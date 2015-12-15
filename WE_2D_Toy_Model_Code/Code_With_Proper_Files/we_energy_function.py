5a2b107802e5617279de5000f61ddca6c3bfc739
Shirley Ahn <sahn1@stanford.edu>
2015-12-15 14:46:52 -0800

    removed unnecessary files

--- WE_2D_Toy_Model_Code/Code_With_Proper_Files/we_energy_function.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def energy_function(x, y):
-    return np.exp(-x**2)+(y)**2
--- WE_2D_Toy_Model_Code/Code_Without_Proper_Files/we_check_state_function_2.py
+++
@@ -1,7 +0,0 @@
-def check_state_function(coordinates):
-    if -1.25 <= coordinates[0] <= -0.75 and -0.05 <= coordinates[1] <= 0.25:
-        return 0
-    elif 0.75 <= coordinates[0] <= 1.25 and -0.05 <= coordinates[1] <= 0.25:
-        return 1
-    else:
-        return -1
