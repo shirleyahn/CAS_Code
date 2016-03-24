ad8d4c470f5aab430e74c559e848d5cd54a34c5e
Shirley Ahn <sahn1@stanford.edu>
2016-03-24 11:24:29 -0700

    minor changes in execute.sh

--- 2D_Toy_Model_Code/Code_Without_Proper_Files/ToyModel1/execute.sh
+++ 2D_Toy_Model_Code/Code_Without_Proper_Files/ToyModel1/execute.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=48:00:00
-#SBATCH --partition=owners
-#SBATCH --qos=normal
+#SBATCH --time=168:00:00
+#SBATCH --partition=mc
+#SBATCH --qos=long
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=2D_Toy_Model
@@ -11,26 +11,24 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sahn1@stanford.edu
 
-### ----------------------------------------------------------------------------------------------------------
-### Bash shell script is specific for SLURM and GROMACS. This can be easily modified for other cluster systems
-### (e.g. PBS) and other molecular dynamics simulation programs (e.g. LAMMPS).
-### ----------------------------------------------------------------------------------------------------------
-
-export MAIN_DIRECTORY=/scratch/users/sahn1/2D_Toy_Model  # TODO: set main directory for CAS simulation
-num_nodes=1  # TODO: set number of nodes requested
-num_cpu=1  # TODO: set number of cores per node
+###
+# Bash shell script is specific for SLURM. This can be easily modified for other cluster systems (e.g. PBS).
+###
 
 echo The master node of this job is `hostname`
-echo The working directory is `echo $MAIN_DIRECTORY`
+echo The working directory is `echo $WORKDIR`
 echo This job runs on the following nodes:
 echo `scontrol show hostname $SLURM_JOB_NODELIST`
 
-cd $MAIN_DIRECTORY
+export MAIN_DIRECTORY=/scratch/users/sahn1/2D_Toy_Model  # TODO: set main directory for WE simulation
+num_nodes=1  # TODO: set number of nodes requested
+num_cpu=1  # TODO: set number of cores per node
 
+cd $MAIN_DIRECTORY
 scontrol show hostname $SLURM_JOB_NODELIST > initial_nodefilelist.txt
 rm -rf nodefilelist.txt
 for i in `seq 1 $num_nodes`;
-do
+do 
     for j in `seq 1 $num_cpu`;
     do
         awk NR==$i initial_nodefilelist.txt >> nodefilelist.txt
