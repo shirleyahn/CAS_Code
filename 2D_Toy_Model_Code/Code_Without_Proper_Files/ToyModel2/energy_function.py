7c4a56e22f0f133332b660adfb300e6fd988c208
Shirley Ahn <sahn1@stanford.edu>
2018-11-12 13:52:13 -0800

    fixed minor things

--- 2D_Toy_Model_Code/Code_Without_Proper_Files/ToyModel2/energy_function.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def energy_function(x, y):
-    return 4.0*(x**2 + y**2 - 1.0)**2*y**2 - np.exp(-4.0*((x - 1.0)**2 + y**2)) - np.exp(-4.0*((x + 1.0)**2 + y**2)) + \
-           np.exp(8.0*(x - 1.5)) + np.exp(-8.0*(x + 1.5)) + np.exp(-4.0*(y + 0.25)) + 0.2*np.exp(-8.0*x**2)
